ea69b75aa04eace08c12e704fa78ba188c74e2a9
alpinocode <alfinohasan629@gmail.com>
2024-01-25 22:02:47 +0700

    membuat fungsi query Kerusakan

---
+++ models/KerusakanModel.js
@@ -0,0 +1,74 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+import Users from "./UserModels.js";
+
+const { DataTypes } = Sequelize
+
+const Kerusakan = db.define('kerusakan', {
+    uuid: {
+        type: DataTypes.STRING,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    tempat: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            len: [1, 150]
+        }
+    },
+    keterangan: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    image_1: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    image_2: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    image_3: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    image_4: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    }
+
+},{
+    freezeTableName: true
+})
+
+Users.hasMany(Kerusakan)
+Kerusakan.belongsTo(Users,{foreignKey: 'userId'})
+
+export default Kerusakan
