0951eca76cc01426d4d50b83b2f9ad588b50a517
alpinocode <alfinohasan629@gmail.com>
2024-01-25 21:51:25 +0700

    Buat fungsi query Datakost

---
+++ models/DataKostmodel.js
@@ -0,0 +1,53 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+import Users from "./UserModels.js";
+
+const { DataTypes } = Sequelize
+
+const DataKost = db.define('tempatkost', {
+    uuid: {
+        type: DataTypes.STRING,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    alamat: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            len: [2, 100]
+        }
+    },
+    desckripsi: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    harga: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            notNull: true
+        }
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    }
+},{
+    freezeTableName: true
+})
+
+Users.hasMany(DataKost)
+DataKost.belongsTo(Users,{foreignKey: 'userId'})
+
+export default DataKost
