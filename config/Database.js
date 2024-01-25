0e81efc5e095a7b093e702470cc571999357ab5d
alpinocode <alfinohasan629@gmail.com>
2024-01-25 21:39:09 +0700

    Configurasi ulang

--- config/Database.js
+++ config/Database.js
@@ -3,6 +3,7 @@ import { Sequelize } from "sequelize";
 // connection mysql
 const db = new Sequelize( 'server_kost', 'root', '', {
     host: process.env.DB_HOST,
+    port: 8111,
     dialect: "mysql"
 })
 
