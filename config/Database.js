4f0aeda70f21a33ef69d6a6b5d89d217e6737f31
alpinocode <alfinohasan629@gmail.com>
2024-01-25 21:30:07 +0700

    configurasi server connection

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 // connection mysql
-const db = new Sequelize( '', 'root', '', {
+const db = new Sequelize( 'server_kost', 'root', '', {
     host: process.env.DB_HOST,
     dialect: "mysql"
 })
