11f8bea15759f6cfbb603b328320bd08dfbb4474
alpinocode <alfinohasan629@gmail.com>
2024-01-26 23:15:15 +0700

    melakukan perubahan pada di sisi git

--- index.js
+++ index.js
@@ -1,14 +1,64 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import session from "express-session"
+import express from "express";
+import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
+// route
+import UserRoute from "./routes/UserRouter.js";
+// import DataSekolahRoute from "./routes/DataSekolahRoute.js";
+// import KerusakanValidasiRoute from "./routes/KerusakanValidasiRoute.js";
+// import PerbaikanValidasiRoute from "./routes/PerbaikanValidasiRoute.js";
+// import KerusakanRoute from "./routes/KerusakanRoute.js";
+// import PerbaikanRoute from "./routes/PerbaikanRoute.js";
+// import AuthRoute from "./routes/AuthRoute.js";
 
-dotenv.config()
+// global
+import db from "./config/Database.js";
+import SequelizeStore from "connect-session-sequelize";
+import fileUpload from "express-fileupload";
 
-const app = express()
+dotenv.config();
 
-app.use(express.json())
+const app = express();
 
-app.listen(process.env.PORT, () => {
-    console.log('Server up and running')
-})
+// const sessionStore = SequelizeStore(session.Store);
+
+// const store = new sessionStore({
+//     db: db
+// });
+
+// 
+(async() => {
+    await db.sync();
+})();
+
+
+app.use(session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    // store: store,
+    cookie: {
+        secure: 'auto',
+    }
+}))
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
+
+app.use(express.json());
+app.use(fileUpload());
+app.use(express.static("public"));
+app.use(UserRoute);
+// app.use(DataSekolahRoute);
+// app.use(AuthRoute);
+// app.use(KerusakanRoute);
+// app.use(KerusakanValidasiRoute);
+// app.use(PerbaikanRoute);
+// app.use(PerbaikanValidasiRoute);
+// store.sync();
+
+app.listen(process.env.APP_PORT, () => {
+    console.log('Server Up and Running...');
+});
