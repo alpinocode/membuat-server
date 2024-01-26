f53f8077cd4a6af41442780bacbf3d56a2327d46
alpinocode <alfinohasan629@gmail.com>
2024-01-26 20:54:02 +0700

    create validasi getDataKostById

--- controllers/DataKostControllers.js
+++ controllers/DataKostControllers.js
@@ -7,7 +7,7 @@ export const getDataKost = async (req, res) => {
     let response
     if(req.role === "admin") {
         response = await DataKost.findAll({
-            attributes: ['nama','alamat', 'desckripsi', 'noHp', 'harga'],
+            attributes: ['name','alamat', 'desckripsi', 'noHp', 'harga'],
             include: {
                 model: User,
                 attributes: ['name', 'email']
@@ -15,7 +15,7 @@ export const getDataKost = async (req, res) => {
         })
     } else {
         response = await DataKost.findAll({
-            attributes: ['nama', 'alamat', 'desckripsi', 'noHp', 'harga'],
+            attributes: ['name', 'alamat', 'desckripsi', 'noHp', 'harga'],
             where: {
                 userId: req.userId
             },
@@ -31,4 +31,42 @@ export const getDataKost = async (req, res) => {
         message: error.message
     })
    }
+}
+
+export const getDataKostById = async (req, res) => {
+    try {
+        const datakost = await DataKost.findOne({
+            where: {
+                uuid: req.params.id
+            }
+        })
+        if(!datakost) return res.status(403).json({ message: "User tidak ditemukan"})
+        let response;
+        if(req.role === "admin") {
+            response = await DataKost.findAll({
+                attributes: ['name', 'alamat', 'desckripsi', 'noHp', 'harga'],
+                where: {
+                    id: datakost.id
+                },
+                include: {
+                    model: User,
+                    attributes: ['name', 'email']
+                }
+            })
+        } else {
+            response = await DataKost.findAll({
+                attributes: ['name', 'alamat', 'desckripsi', 'noHp', 'harga'],
+                where: {
+                    [Op.and]: [{id: datakost.id}, {userId: req.userId}]
+                },
+                include: {
+                    model: User,
+                    attributes: ['name', 'email']
+                }
+            })   
+        }
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
 }
