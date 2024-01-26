8511e4393ede19be04f48da28bf33ae99dc37805
alpinocode <alfinohasan629@gmail.com>
2024-01-26 21:52:10 +0700

    membuat routes userRoute.js

---
+++ routes/UserRouter.js
@@ -0,0 +1,17 @@
+import express from "express"
+import {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+}from "../controllers/UserControllers"
+import { verifyUser, adminOnly } from "../middleware/Auth"
+
+const router = express.Router()
+
+router.get('/user',getUsers)
+router.get('/user/:id', verifyUser, adminOnly, getUserById)
+router.post('/user', createUser)
+router.patch('/user/:id', verifyUser, adminOnly, updateUser)
+router.delete('/user/:id', verifyUser, adminOnly, deleteUser)
