a1d9d3625e9d14d575e6f327f119c2ce8f5e4a26
alpinocode <alfinohasan629@gmail.com>
2024-01-28 01:33:03 +0700

    pemanggilan fungsi ke server

--- index.js
+++ index.js
@@ -4,29 +4,40 @@ import session from "express-session";
 import dotenv from "dotenv";
 // route
 import UserRoute from "./routes/UserRouter.js";
-
+import DataKostRoute from "./routes/DataKostRoute.js"
+import KerusakanRoute from "./routes/KerusakanRoute.js"
+import KerusakanValidasi from "./routes/KerusakanValidasi.js"
+import PerbaikanRoute from "./routes/PerbaikanRoute.js"
+import PerbaikanValidasi from "./routes/PerbaikanValidasiRouter.js"
+import AuthRoute from "./routes/AuthRoute.js"
 
 // global
 import db from "./config/Database.js";
 import SequelizeStore from "connect-session-sequelize";
 import fileUpload from "express-fileupload";
 
+
 dotenv.config();
 
 const app = express();
 
+const sessionStore = SequelizeStore(session.Store)
 
-// 
-(async() => {
-    await db.sync();
-})();
+const store = new sessionStore({
+    db: db
+})
+
+
+// (async() => {
+//     await db.sync();
+// })();
 
 
 app.use(session({
     secret: process.env.SESS_SECRET,
     resave: false,
     saveUninitialized: true,
-    // store: store,
+    store: store,
     cookie: {
         secure: 'auto',
     }
@@ -41,6 +52,12 @@ app.use(express.json());
 app.use(fileUpload());
 app.use(express.static("public"));
 app.use(UserRoute);
+app.use(DataKostRoute)
+app.use(KerusakanRoute)
+app.use(KerusakanValidasi)
+app.use(PerbaikanRoute)
+app.use(PerbaikanValidasi)
+app.use(AuthRoute)
 
 
 app.listen(process.env.APP_PORT, () => {
